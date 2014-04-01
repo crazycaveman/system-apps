8ccfe7ebd57be44ae543fade63518ab24b999550
U-Hulk\Joe <Joe@Hulk.(none)>
2014-03-31 22:39:00 -0400

    Added files fot signing. Also added .gitignore

---
+++ make_zip.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Create and sign a zip file suitable for flashing on Android
+
+zip -r system-apps.zip * -x make_zip.sh signing README*
+java -Xmx1024m -jar signing/signapk.jar -w signing/testkey.x509.pem signing/testkey.pk8 system-apps.zip system-apps-signed.zip
