9f451d861f1eb441d2078965733e6139956bc08f
U-Hulk\Joe <crazycaveman@gmail.com>
2014-04-01 15:05:03 -0400

    Exclude zip from flashable zip file creation.

--- make_zip.sh
+++ make_zip.sh
@@ -2,5 +2,5 @@
 
 # Create and sign a zip file suitable for flashing on Android
 
-zip -r system-apps.zip * -x make_zip.sh signing README*
+zip -r system-apps.zip * -x make_zip.sh signing README* system-apps*.zip
 java -Xmx1024m -jar signing/signapk.jar -w signing/testkey.x509.pem signing/testkey.pk8 system-apps.zip system-apps-signed.zip
